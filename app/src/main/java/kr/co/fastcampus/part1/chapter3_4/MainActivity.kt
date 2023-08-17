b2cb194582f05952273e70d3b4fff3a5607babf7
Oracle-Philip <philipy075@gmail.com>
2023-08-17 23:09:58 +0900

    Part1_Row

--- app/src/main/java/kr/co/fastcampus/part1/chapter3_4/MainActivity.kt
+++ app/src/main/java/kr/co/fastcampus/part1/chapter3_4/MainActivity.kt
@@ -4,14 +4,24 @@ import android.graphics.Paint.Align
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kr.co.fastcampus.part1.chapter3_4.R
 import kr.co.fastcampus.part1.chapter3_4.ui.theme.RowTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,20 +43,40 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun RowEx() {
     Row(
-        modifier = Modifier.height(40.dp),
-        verticalAlignment = Alignment.Top
+        //horizontalArrangement = Arrangement.SpaceEvenly,
+        verticalAlignment = Alignment.Bottom,
+        modifier = Modifier
+            .height(40.dp)
+            .width(200.dp)
     ) {
         Text(
             text = "첫 번째!",
-            modifier = Modifier.align(Alignment.Top),
+            modifier = Modifier
+                .align(Alignment.Top)
+                .weight(3f)
+                .background(Color.Magenta),
+            textAlign = TextAlign.End,
         )
-        Text(
-            text = "두 번째!",
-            modifier = Modifier.align(Alignment.CenterVertically)
+//        Text(
+//            text = "두 번째!",
+//            modifier = Modifier
+//                .weight(1f)
+//            //modifier = Modifier.align(Alignment.CenterVertically)
+//        )
+        Icon(
+            imageVector = Icons.Filled.AccountBox,
+            contentDescription = "추",
+            modifier = Modifier
+                .weight(1f)
+                .background(Color.Cyan),
         )
         Text(
             text = "세 번째!",
-            modifier = Modifier.align(Alignment.Bottom)
+            modifier = Modifier
+                .weight(3f)
+                .background(Color.Blue),
+            textAlign = TextAlign.Center,
+            //modifier = Modifier.align(Alignment.Bottom)
         )
     }
 

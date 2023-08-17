0fd78a767c9237625ff2beaa378914a4538ffe23
Oracle-Philip <philipy075@gmail.com>
2023-08-18 00:00:17 +0900

    part1_Column
    - Column 컴포넌트의 진행방향은 세로이다.
    - Alignment는 가로로 적용된다.
    - Arrangement는 세로로 적용된다.(동일)
    - 참고) Box 컴포넌트는 가로 세로 Aligment 설정가능하다

--- app/src/main/java/kr/co/fastcampus/part1/chapter3_4/MainActivity.kt
+++ app/src/main/java/kr/co/fastcampus/part1/chapter3_4/MainActivity.kt
@@ -1,107 +1,80 @@
-package kr.co.fastcampus.part1.chapter3_7
+package kr.co.fastcampus.part1.chapter3_8
 
-import android.graphics.Paint.Align
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.Icon
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import kr.co.fastcampus.part1.chapter3_4.R
-import kr.co.fastcampus.part1.chapter3_4.ui.theme.RowTheme
+import kr.co.fastcampus.part1.chapter3_4.ui.theme.ColumnTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            RowTheme {
-                RowEx()
+            ColumnTheme {
+                ColumnEx()
             }
         }
     }
 }
 
 /**
- * Row는 컴포넌트를 가로로 이어서 배치한다.
- * modifier width를 정하지 않으면 컨텐츠 사이즈만큼 적용된다
- * Row는 기본적으로 vertical alignment 적용
+ * - Row & Column은 1차원으로만 Alignment 할 수 있고,
+ * - Box는 2차원 Alignment 가능하다.
+ *   - Column에 경우 Horizontal하게만 Alignment를 할 수 있다!
+ *   - Row는 Vertical하게만 Alignment를 할 수 있다!
+ *   - Row는 가로배치이므로 Alignment는 세로로 적용된다!
+ *   - Box 컴포넌트에서는 Alignment가 Vertical, Horizontal 둘다 된다!
  */
 @Composable
-fun RowEx() {
-    Row(
-        //horizontalArrangement = Arrangement.SpaceEvenly,
-        verticalAlignment = Alignment.Bottom,
-        modifier = Modifier
-            .height(40.dp)
-            .width(200.dp)
+fun ColumnEx() {
+    Column(
+        modifier = Modifier.size(100.dp),
+        horizontalAlignment = Alignment.End,
+        /**
+         * Alignment는 레이아웃의 방향과 반대, Column이 세로배치이므로 Alignment는 가로로 적용된다.
+         * Alignment는 2D, 1D에 Horizontal Center, vertical Center 등 고려할 것들이 있다.
+         */
+
+        verticalArrangement =  Arrangement.Center
+        /**
+         * Arrangement는 레이아웃의 진행방향과 같은 방향으로 적용된다.
+         */
+
+
     ) {
         Text(
-            text = "첫 번째!",
-            modifier = Modifier
-                .align(Alignment.Top)
-                .weight(3f)
-                .background(Color.Magenta),
-            textAlign = TextAlign.End,
-        )
-//        Text(
-//            text = "두 번째!",
-//            modifier = Modifier
-//                .weight(1f)
-//            //modifier = Modifier.align(Alignment.CenterVertically)
-//        )
-        Icon(
-            imageVector = Icons.Filled.AccountBox,
-            contentDescription = "추",
-            modifier = Modifier
-                .weight(1f)
-                .background(Color.Cyan),
+            text = "첫 번째",
+            modifier = Modifier.align(Alignment.CenterHorizontally)
         )
         Text(
-            text = "세 번째!",
-            modifier = Modifier
-                .weight(3f)
-                .background(Color.Blue),
-            textAlign = TextAlign.Center,
-            //modifier = Modifier.align(Alignment.Bottom)
+            text = "두 번째"
+        )
+        Text(
+            text = "세 번째",
+            modifier = Modifier.align(Alignment.Start)
         )
     }
 
-    // 스텝 1: 각 Text의 modifier에 align을 설정합시다.
-    // Alignment.Top, CenterVertically, Bottom을 지정해봅시다.
+    // 스텝 1: horizontalAlignment를 Column에 적용해봅시다.
 
-    // 스텝 2: Row에 verticalAlignment를 설정해봅시다.
-    // Text에 align을 사용할 때와 쓰이는 값이 같습니다.
+    // 스텝 2: Column에 verticalArrangement를 지정해봅시다.
+    // SpaceAround, SpaceEvenly, SpaceBetween도 해봅시다.
 
-    // 스텝 3: Row의 width를 200dp 정도로 설정합시다.
-    // Row에 horizontalArrangement에 Arrangement.Center를
-    // 설정해봅시다. Start, End, SpaceAround, SpaceBetween
-    // SpaceEvenly를 설정해봅시다.
-
-    // 스텝 4: horizontalArrangement를 제거하고 각 Text에
-    // Modifier.weight를 설정합시다. 각 항목의 weight 값을 바꾸어
-    // 보세요.
-
-    // 스텝 5: Text 대신 Icon을 하나 넣어봅시다.
+    // 스텝 3: Text에 Modifier.align을 사용해 봅시다.
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    RowTheme {
-        RowEx()
+    ColumnTheme {
+        ColumnEx()
     }
 }
--- app/src/main/java/kr/co/fastcampus/part1/chapter3_4/ui/theme/Theme.kt
+++ app/src/main/java/kr/co/fastcampus/part1/chapter3_4/ui/theme/Theme.kt
@@ -28,7 +28,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun RowTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+fun ColumnTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {

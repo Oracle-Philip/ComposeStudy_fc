b84e99aed0c1fd210c4b06c7464cce056ff1c0c8
PhilipY <philipy075@gmail.com>
2023-08-19 16:53:14 +0900

    part1_TextField

--- part1-chapter3-14/app/src/main/java/kr/co/fastcampus/part1/chapter3_14/MainActivity.kt
+++ part1-chapter3-14/app/src/main/java/kr/co/fastcampus/part1/chapter3_14/MainActivity.kt
@@ -4,9 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -25,9 +34,40 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting() {
-    Column(modifier = Modifier.padding(16.dp)) {
+    //val (getter, setter) = rememberSaveable { mutableStateOf(String) }
+
+    var name by rememberSaveable { mutableStateOf("Tom") }
+
+    Column(modifier = Modifier
+        .padding(16.dp)) {
         // 스텝 1: TextField를 Text위에 만듭니다.
         // value와 onValueChanged는 비워둡시다.
+
+//        TextField(
+//            value = name,
+//            onValueChange = {
+//                name = it
+//            }
+//        )
+
+//        TextField(
+//            value = name,
+//            label = {
+//                Text("이름")
+//            },
+//            onValueChange = {
+//                name = it
+//            }
+//        )
+        OutlinedTextField(
+            value = name,
+            label = {
+                Text("이름")
+            },
+            onValueChange = {
+                name = it
+            }
+        )
 
         // 스텝 2: Text에 Android 대신 TextField 입력을
         // 출력하게 합시다. mutableStateOf("") 필드를
@@ -41,7 +81,15 @@ fun Greeting() {
 
         // 스텝 5: TextField를 OutlinedTextField로 변경해봅시다.
 
-        Text(text = "Hello Android")
+        Spacer(modifier = Modifier.size(8.dp))
+
+        Text(text = "Hello $name")
+
+        Spacer(modifier = Modifier.size(8.dp))
+
+        OutlinedButton(onClick = {}) {
+
+        }
     }
 }
 
